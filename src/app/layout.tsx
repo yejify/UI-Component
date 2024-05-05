e62894de677da42f2799b55074db275e6ac61623
yeji <skql4128@naver.com>
2024-05-05 20:14:21 +0900

    아코디언1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.scss'
-import Gnb from './gnb'
+import type { Metadata } from 'next';
+import './globals.scss';
+import Gnb from './gnb';
 
 export const metadata: Metadata = {
-  title: 'UI요소모음 | FE재남',
+  title: 'UI요소모음',
   description: 'Vanilla / React로 UI요소 만들기',
-}
+};
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="ko">
+    <html lang='ko'>
       <body>
         <Gnb />
         <main>{children}</main>
       </body>
     </html>
-  )
-}
-export default Layout
+  );
+};
+export default Layout;
