5b75ffe031b11a3a230503ef1170deff1549e227
yeji <skql4128@naver.com>
2024-05-22 16:04:35 +0900

    measureLines 분리

--- src/components/textBox/1_r.tsx
+++ src/components/textBox/1_r.tsx
@@ -1,25 +1,7 @@
 import { SyntheticEvent, useState } from 'react';
 import cx from './cx';
+import { measureLines } from '@/service/util';
 
-const measureLines = (elem: HTMLTextAreaElement, val: string) => {
-  if (!elem || !val) return 0;
-  const canvas = document.createElement('canvas');
-  const canvasContext: CanvasRenderingContext2D = canvas.getContext('2d')!;
-  const style = window.getComputedStyle(elem);
-  canvasContext.font = `${style.getPropertyValue(
-    'font-size'
-  )} ${style.getPropertyValue('font-family')}`;
-  const measuredLines = val.split('\n').reduce((r, c) => {
-    return (
-      r +
-        Math.max(
-          Math.ceil(canvasContext.measureText(c).width) / elem!.offsetWidth
-        ),
-      1
-    );
-  }, 0);
-  return measuredLines;
-};
 const TextBox1 = () => {
   const [text, setText] = useState('');
   const [lines, setLines] = useState(3);
