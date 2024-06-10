59449beb59f7db34d76ee57f26cbede9f9ff4c34
yeji <skql4128@naver.com>
2024-06-11 00:04:09 +0900

    LineClamp2 - clone

--- src/components/lineClamp/index.tsx
+++ src/components/lineClamp/index.tsx
@@ -1,11 +1,12 @@
 import LineClamp1 from './1_r';
+import LineClamp2 from './2_r';
 import cx from './cx';
 
 const LineClamps = () => (
   <div className={cx('LineClamps')}>
     <h2>여러줄 말줄임</h2>
     <LineClamp1 />
-    <LineClamp1 />
+    <LineClamp2 />
   </div>
 );
 
