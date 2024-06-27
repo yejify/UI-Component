80b5220b51a8cc9009bfb9192ace0ea9165df28e
yeji <skql4128@naver.com>
2024-06-27 11:58:52 +0900

    IntersectionOserver

---
+++ src/hook/useIntersectionObserver.ts
@@ -0,0 +1,25 @@
+import { RefObject, useEffect, useRef, useState } from 'react';
+
+const useIntersectionOserver = (
+  elemRef: RefObject<HTMLElement>,
+  options: IntersectionObserverInit
+) => {
+  const observerRef = useRef<IntersectionObserver>();
+  const [entries, setEntries] = useState<IntersectionObserverEntry[]>([]);
+
+  useEffect(() => {
+    const node = elemRef.current;
+
+    if (!node) return;
+    observerRef.current = new IntersectionObserver(setEntries, options);
+    observerRef.current.observe(node);
+
+    return () => {
+      observerRef.current?.disconnect();
+    };
+  }, [elemRef, options]);
+
+  return { entries, observerRef };
+};
+
+export default useIntersectionOserver;
