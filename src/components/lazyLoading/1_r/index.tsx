6aef9c115808d628883cb8b2b92b2fdabf36e8da
yeji <skql4128@naver.com>
2024-07-01 09:34:43 +0900

    로딩 완료시 클래스 삭제

--- src/components/lazyLoading/1_r/index.tsx
+++ src/components/lazyLoading/1_r/index.tsx
@@ -1,7 +1,7 @@
 import useIntersectionOserver from '@/hook/useIntersectionObserver';
 import cx from '../cx';
 import data from '../data';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const ioOptions: IntersectionObserverInit = {
   threshold: 0,
@@ -17,18 +17,29 @@ const LazyImage = ({
   height: number;
 }) => {
   const imgRef = useRef<HTMLImageElement>(null);
+  const [loaded, setLoaded] = useState(false);
   const { entries, observerRef } = useIntersectionOserver(imgRef, ioOptions);
 
   useEffect(() => {
     const isVisible = entries[0]?.isIntersecting;
-
     if (isVisible) {
+      const onLoad = () => {
+        setLoaded(true);
+      };
+      imgRef.current!.addEventListener('load', onLoad, { once: true });
       imgRef.current!.setAttribute('src', src);
       observerRef.current?.disconnect();
     }
   }, [src, entries]);
 
-  return <img className={cx('lazy')} width={width} height={height} alt='' />;
+  return (
+    <img
+      className={cx({ lazy: !loaded })}
+      width={width}
+      height={height}
+      alt=''
+    />
+  );
 };
 
 const LazyLoad1 = () => {
